afe1e7e039119c5f6521b630d352c91d80941242
Ncia89 <nicholas.ciarelli@gmail.com>
2019-11-15 14:31:25 -0500

    Added 3 components, an Event interface, and an Events service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    EventListComponent,
+    BucketlistPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
