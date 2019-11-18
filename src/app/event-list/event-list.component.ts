cba0023d4d68766cae49e8296409f0d3225fde82
Ncia89 <nicholas.ciarelli@gmail.com>
2019-11-17 20:14:48 -0500

    Got the events list to reload on a search submit. Not sure if it is the prefered method. Will have to look into it.

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventsService } from '../services/events.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.css']
 })
-export class EventListComponent implements OnInit {
+export class EventListComponent implements OnInit, OnDestroy {
   events: any[] = [];
   noEvents: boolean = false;
 
-  constructor(private eventsService: EventsService) {}
+  // For catching when trying to route to the page even if there already
+  navigationSubscription;
+
+  constructor(private eventsService: EventsService, private router:Router) {
+    // Part of trying to get the page to reload on routing to it
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.initialiseInvites();
+      }
+    });
+  }
 
   ngOnInit() {
-    // Call for the initial list of events to show on the page
+    // Call for the list of events to show on the page
     this.eventsService
       .getEvents()
       .subscribe(data => {
@@ -25,5 +37,30 @@ export class EventListComponent implements OnInit {
         }
         console.log(this.events);
       });
+  }
+
+  initialiseInvites() {
+    // Set default values and re-fetch any data you need.
+    // Call for the list of events to show on the page
+    this.eventsService
+      .getEvents()
+      .subscribe(data => {
+        if("_embedded" in data){
+          this.events = data._embedded.events;
+        }
+        if(this.events.length === 0){
+          this.noEvents = true;
+        }
+        console.log(this.events);
+      });
+  }
+  
+  ngOnDestroy() {
+     // avoid memory leaks here by cleaning up after ourselves. If we  
+     // don't then we will continue to run our initialiseInvites()   
+     // method on every navigationEnd event.
+     if (this.navigationSubscription) {  
+        this.navigationSubscription.unsubscribe();
+     }
   }
 }
